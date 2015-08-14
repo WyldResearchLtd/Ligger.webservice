1e72710582089a94ece9957e52e362a07d833e3c
genemyers <gene@fezzee.com>
2015-08-14 14:05:38 +0100

    Added HMAC to POST and improved postgres  query and connection error handling

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 Ligger.webservice
 -----------------
 
+AWS Ligger.webservice
+
+http://www.bennadel.com/blog/2321-how-i-got-node-js-running-on-a-linux-micro-instance-using-amazon-ec2.htm
+
+
+create VM Ubuntu server 14.04 (micro)
+open port 5433
+assign elastic IP (52.16.121.156) - use DNS made easy to create an A record for this to:   ligger-api.fezzee.net
+edit .ssh config file
+ssh prod-liggerapi
+
+sudo apt-get update
+sudo apt-get install git
+
+git clone http://github.com/fezzee/Ligger.webservice
+
+
+sudo apt-get install nodejs
+sudo apt-get install npm
+cd Ligger.webservice
+npm install
+nodejs server.js
+
+NOTE- changed server.js to that listen is only specifying the port as a parameter, instead of the port and host
+
+# non core services should not operate below port 1024, so we use port forwarding
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 5433
+
+#  if you install Node.js from a package manager your bin may be called nodejs so you just need to symlink it 
+
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+
+#Create Start and Stop scripts for forever 
+
+nano  start
+————————————————————————————————————————————————————————————
+ #!/bin/bash
+
+ # Invoke the Forever module (to START our Node.js server).
+ ./node_modules/forever/bin/forever \
+ start \
+ -al forever.log \
+ -ao out.log \
+ -ae err.log \
+ server.js
+————————————————————————————————————————————————————————————
+chmod +x ./start
+
+nano stop
+————————————————————————————————————————————————————————————
+#!/bin/bash
+
+ # Invoke the Forever module (to STOP our Node.js server).
+ ./node_modules/forever/bin/forever stop server.js
+————————————————————————————————————————————————————————————
+chmod +x ./stop
+
+
+NOTE- Again refer to http://www.bennadel.com/blog/2321-how-i-got-node-js-running-on-a-linux-micro-instance-using-amazon-ec2.htm
+
--- server.js
+++ server.js
@@ -7,55 +7,103 @@
  *************************************************************************************************************/
 var pg = require("pg")
 var http = require("http")
+var crypto = require("crypto");
 var port = 5433;
 var host = '127.0.0.1';
+var sharedSecret = '608169da637a58ac0bff23895b58f8de5ef982a5a30f5477e2fdea27c5bdef8d5b0b13bfc8c2c77c';
 //var strDBconn = "pg://postgres:postgres@localhost:5432/ligger";
 var strDBconn = "pg://fezzee:f33ZZAR1@ligger.fezzee.net:5432/ligger";
 
 //POST
 var insert_records = function(req, res) {
-   console.log("In insert");
-   // Connect to DB
-   //var strDBconn = "pg://postgres:postgres@"+ host+ ":" + port + "/ligger";
-   var client = new pg.Client(strDBconn);
-   client.connect(); 
-
+   console.log("POST-insert_records");
+   
    var data = "";
 
     //console.log("POST Request obj: " + req);
     req.on('data', function(chunk) {
 	      data += chunk.toString();
-	      console.log("Received body data:");
-	      console.log(chunk.toString());
+	      console.log("Received body data");
+	      //console.log(chunk.toString());
 	});
 
 	req.on('end', function() {
 	      // empty 200 OK response for now
 	      res.writeHead(200, "OK", {'Content-Type': 'text/html'});
 	      res.end();
-	      //data = "{\"Audio\":0}";
 	      console.log("Data end.");
-	      //add to DB here
+	      //Check hmac and Add to DB 
 	        try {
-		        var jsonData = JSON.parse(data);
-	            client.query("INSERT INTO scores(name, userID, deviceID, score, level, datetime, log, timeremaining) values($1, $2, $3, $4, $5, $6, $7, $8)", 
-	              [jsonData.scoreObj.scoreName, 
-	               jsonData.scoreObj.UserGUID, 
-	               jsonData.scoreObj.DeviceGUID, 
-	               jsonData.scoreObj.scoreValue, 
-	               jsonData.scoreObj.scoreLevel, 
-	               jsonData.scoreObj.scoreDate, 
-	               jsonData, 
-	               jsonData.scoreObj.timeRemaining]);
-	            console.log("Inserted Score data");
+		       //check HMAC
+			   //console.log("HEADERS: " +JSON.stringify(req.headers));
+			   var hmac = req.headers['content-hmac'];  //needs to be lowercase because node.js returns headers as json
+			   console.log("content-hmac: " + hmac);
+			   //we do not have to worry about replay attacks, because the
+			   var query = "post /\nhost: http://" + req.headers['host'] + " /\nbody: " + data;
+			   var signature = crypto.createHmac("sha256", sharedSecret).update(query).digest("hex");
+			   console.log("   Signature: " + signature);
+			   //only if true connect to DB
+			   if (hmac == signature)
+			   {
+		           //Add to DB 
+		           var jsonData = JSON.parse(data);
+		           pg.connect(strDBconn, function(err, client, done) {
+	               var query = client.query("INSERT INTO scores(name, userID, deviceID, score, level, datetime, log, timeremaining) values($1, $2, $3, $4, $5, $6, $7, $8)", 
+	                  [jsonData.scoreObj.scoreName, 
+	                   jsonData.scoreObj.UserGUID, 
+	                   jsonData.scoreObj.DeviceGUID, 
+	                   jsonData.scoreObj.scoreValue, 
+	                   jsonData.scoreObj.scoreLevel, 
+	                   jsonData.scoreObj.scoreDate, 
+	                   jsonData, 
+	                   jsonData.scoreObj.timeRemaining
+	                  ],function(err, result){
+						if(err) {
+					          console.log("INSERT INTO Query: " + err);
+					    }
+	                });
+	                console.log("Attempt to Insert Score data: UID" + jsonData.scoreObj.UserGUID + " DID: " + jsonData.scoreObj.DeviceGUID + " Date: " + jsonData.scoreObj.scoreDate);
+	                //close connection on end
+	                query.on('end', function() {
+				            client.end();
+				            console.log("INSERT INTO Sucessful, Connection Closed");
+				     });
+
+				     // Handle Connection Errors
+				     if(err) {
+				          console.log("Connection (INSERT INTO)  Error:" + err);
+				      }
+				     
+                  });
+               } else {
+					console.log("ERROR: Signatures did not match");
+					console.log(data);
+               }
 		    } catch (e) {
-			    console.log("Bad POST Data");
+			    console.log("insert_records EXCEPTION: " + e);
 		        return false;
 		    }
 	});
     
    // Create table if it doesn't exist - NOTE- DB MUST exist!
-   client.query("CREATE TABLE IF NOT EXISTS scores(userID text NOT NULL, deviceID text NOT NULL, name text, score integer, level integer, datetime text, log text, timeremaining integer, CONSTRAINT users_pkey PRIMARY KEY (userID, deviceID, datetime))");
+   pg.connect(strDBconn, function(err, client, done) {
+   		var query = client.query("CREATE TABLE IF NOT EXISTS scores(userID text NOT NULL, deviceID text NOT NULL, name text, score integer, level integer, datetime text, log text, timeremaining integer, CONSTRAINT users_pkey PRIMARY KEY (userID, deviceID, datetime))",
+         	function(err, result){
+				if(err) {
+			          console.log("CREATE TABLE IF NOT EXISTS Query: " + err);
+			    }
+         });
+        
+         query.on('end', function() {
+	            client.end();
+	            console.log("CREATE TABLE IF NOT EXISTS Sucessful, Connection Closed");
+	     });
+
+	     // Handle Connection Errors
+	     if(err) {
+	          console.log("Connection (CREATE TABLE IF NOT EXISTS) Error:" + err);
+	      }
+   });
    // Write output
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.write("POST data initialised\n");
@@ -66,30 +114,43 @@ var insert_records = function(req, res) {
 
   // GET
   var list_records = function(req, res) {
-  console.log("In listing records");
+  console.log("GET-list_records");
+  
+  pg.connect(strDBconn, function(err, client, done) {
+  	// Create table if it doesn't exist - NOTE- DB MUST exist!
+  	client.query("CREATE TABLE IF NOT EXISTS scores(userID text NOT NULL, deviceID text NOT NULL, name text, score integer, level integer, datetime text, log text, timeremaining integer, CONSTRAINT users_pkey PRIMARY KEY (userID, deviceID, datetime))");
 
-  // Connect to DB
-  //var strDBconn = "pg://postgres:postgres@"+ host+ ":" + port + "/ligger";
-  var client = new pg.Client(strDBconn);
-  client.connect(); 
+  	// Select the rows in the table, ordered and limited
+  	var query = client.query("SELECT name, userID, deviceID, score, level, datetime FROM scores ORDER BY score DESC, userID DESC LIMIT 3");
 
-  // Create table if it doesn't exist - NOTE- DB MUST exist!
-  client.query("CREATE TABLE IF NOT EXISTS scores(userID text NOT NULL, deviceID text NOT NULL, name text, score integer, level integer, datetime text, log text, timeremaining integer, CONSTRAINT users_pkey PRIMARY KEY (userID, deviceID, datetime))");
-
-  // Select the rows in the table, ordered and limited
-  var query = client.query("SELECT name, userID, deviceID, score, level, datetime FROM scores ORDER BY score DESC, userID DESC LIMIT 3");
-  //ASC or DESC
-  query.on("row", function (row, result) {
+  	query.on("row", function (row, result) {
     	 result.addRow(row);
-   });
-   query.on("end", function (result) {
+   	});
+
+   	query.on("end", function (result) {
       
-   // On end JSONify and write the results to console and to HTML output
-   console.log(JSON.stringify(result.rows, null, "    "));
+        client.end();
+        console.log("SELECT Sucessful, Connection Closed");
+        
+   		
+   		//console.log(JSON.stringify(result.rows, null, "    "));
+
+		// On end JSONify and write the results to HTML output
     	res.writeHead(200, {'Content-Type': 'text/plain'});
     	res.write(JSON.stringify(result.rows) + "\n");
     	res.end();
-   });
+    });
+
+    query.on('error', function(err) {
+	      //cannot use the err function inline in client.query, because you'll get double results
+	      console.log("Query (SELECT): " + err);
+	});
+
+    // Handle Connection Errors
+    if(err) {
+         console.log("Connection (SELECT) Error:" + err);
+     }
+  });
 }
 
 // Create server 
